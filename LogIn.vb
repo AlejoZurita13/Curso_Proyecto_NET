d7250a18869f28a42489c4b33933aefebc0c0f9a
Alejo <alejozuritar@gmail.com>
2021-09-30 23:19:47 -0300

    Se agrego un nuevo formulario

--- LogIn.vb
+++ LogIn.vb
@@ -3,4 +3,19 @@
         Close()
 
     End Sub
+
+    Private Sub TxtIngreseUsuario_TextChanged(sender As Object, e As EventArgs) Handles TxtIngreseUsuario.TextChanged
+
+    End Sub
+
+    Private Sub TxtIngreseUsuario_Click(sender As Object, e As EventArgs) Handles TxtIngreseUsuario.Click
+        TxtIngreseUsuario.Text = ""
+        TxtIngreseUsuario.Focus()
+
+    End Sub
+
+    Private Sub txtContraseña_Click(sender As Object, e As EventArgs)
+        txtContraseña.Text = ""
+        txtContraseña.Focus()
+    End Sub
 End Class
---
+++ Usuarios.Designer.vb
@@ -0,0 +1,66 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Usuarios
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.lblUsuarios = New System.Windows.Forms.Label()
+        Me.Panel1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Panel1
+        '
+        Me.Panel1.Controls.Add(Me.lblUsuarios)
+        Me.Panel1.Dock = System.Windows.Forms.DockStyle.Top
+        Me.Panel1.Location = New System.Drawing.Point(0, 0)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(614, 57)
+        Me.Panel1.TabIndex = 0
+        '
+        'lblUsuarios
+        '
+        Me.lblUsuarios.AutoSize = True
+        Me.lblUsuarios.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblUsuarios.Location = New System.Drawing.Point(267, 13)
+        Me.lblUsuarios.Name = "lblUsuarios"
+        Me.lblUsuarios.Size = New System.Drawing.Size(80, 20)
+        Me.lblUsuarios.TabIndex = 0
+        Me.lblUsuarios.Text = "Usuarios"
+        '
+        'Usuarios
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(614, 309)
+        Me.Controls.Add(Me.Panel1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.Name = "Usuarios"
+        Me.Text = "Usuarios"
+        Me.Panel1.ResumeLayout(False)
+        Me.Panel1.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents lblUsuarios As Label
+End Class
