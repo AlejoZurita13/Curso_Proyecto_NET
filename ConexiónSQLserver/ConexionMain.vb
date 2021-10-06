14476d942cc8027e589f51a9f65a5a040204d431
Alejo <alejozuritar@gmail.com>
2021-10-05 22:17:09 -0300

    Configuración de la conexión con server

--- ConexiónSQLserver/ConexionMain.vb
+++ ConexiónSQLserver/ConexionMain.vb
@@ -1,3 +1,18 @@
-﻿Module ConexionMain
+﻿Imports System.Data.SqlClient
 
+Module ConexionMain
+    Public conexion As New SqlConnection("Data Source=DESKTOP-I93TGEL\SQLEXPRESS;Initial Catalog=BaseDatosCursoProyecto;Integrated Scurity=True")
+
+    Sub abrir()
+        If conexion.State = 0 Then
+            conexion.Open()
+
+        End If
+    End Sub
+
+    Sub cerrar()
+        If conexion.State = 1 Then
+            conexion.Close()
+        End If
+    End Sub
 End Module
